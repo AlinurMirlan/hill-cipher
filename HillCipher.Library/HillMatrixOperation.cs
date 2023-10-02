f8300778075d6c86778c4f3b7404880277c231df
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2023-10-02 19:02:03 +0600

    refactor: matrix inverse method farmed out into a separate class

---
+++ HillCipher.Library/HillMatrixOperation.cs
@@ -0,0 +1,64 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HillCipher.Library;
+
+public class HillMatrixOperation
+{
+    private char[] _alphabet;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public HillMatrixOperation(char[] alphabet)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+        Alphabet = alphabet;
+    }
+
+    public char[] Alphabet
+    {
+        get => _alphabet;
+        set
+        {
+            if (value.Length <= 0)
+                throw new ArgumentException($"{nameof(Alphabet)} must not be empty.");
+
+            _alphabet = value;
+        }
+    }
+
+    public Matrix<double> Inverse(Matrix<double> key)
+    {
+        int alphabetLength = _alphabet.Length;
+        Matrix<double> adjugateKey = key.Adjugate() % alphabetLength;
+        NormalizeMatrix(adjugateKey);
+
+        double keyDeterminant = Math.Round(key.Determinant()) % alphabetLength;
+        if (keyDeterminant < 0)
+            keyDeterminant += alphabetLength;
+
+        int inverseDeterminant = 0;
+        int inverseAlphabet = 0;
+        Gcd.CommonDivisorExtended((int)keyDeterminant, alphabetLength, ref inverseDeterminant, ref inverseAlphabet);
+        if (inverseDeterminant < 0)
+            inverseDeterminant += alphabetLength;
+
+        return inverseDeterminant * adjugateKey % alphabetLength;
+    }
+
+    private void NormalizeMatrix(Matrix<double> matrix)
+    {
+        for (int i = 0; i < matrix.RowCount; i++)
+        {
+            for (int j = 0; j < matrix.ColumnCount; j++)
+            {
+                matrix[i, j] = Math.Round(matrix[i, j]);
+                if (matrix[i, j] < 0)
+                    matrix[i, j] += _alphabet.Length;
+            }
+        }
+    }
+}
