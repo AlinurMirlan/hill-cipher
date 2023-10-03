b203386e2dbb2e45408d5fea4f804811062f1249
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2023-10-03 18:05:15 +0600

    chore: removing unnecessary imports

--- HillCipher.Library/MatrixExtensions.cs
+++ HillCipher.Library/MatrixExtensions.cs
@@ -1,11 +1,4 @@
 ﻿using MathNet.Numerics.LinearAlgebra;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HillCipher.Library;
 
